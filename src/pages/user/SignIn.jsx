88ef858ade322cf5fbe963def26e3529fc8e2f15
Umashnkar Uikey <umashankaruikey325@gmail.com>
2025-05-14 01:05:28 +0530

    feat: refactor SignIn component and update layout and styling

--- src/pages/user/SignIn.jsx
+++ src/pages/user/SignIn.jsx
@@ -1,18 +1,25 @@
-const SignUp = () => {
+import React from "react";
+
+const SignIn = () => {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100 p-4">
+    <div className="min-h-screen h-300px flex items-center justify-center bg-gray-100 p-4">
       <div className="flex flex-col md:flex-row w-full max-w-5xl bg-white shadow-xl rounded-2xl overflow-hidden">
-        
         {/* Left Section */}
         <div className="md:w-1/2 bg-[url('https://images.unsplash.com/photo-1744029829181-ad19c2ee248b?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxfHx8ZW58MHx8fHx8')] bg-cover bg-center text-white flex flex-col justify-between p-8">
           <div>
-            <h1 className="text-3xl font-bold leading-tight mb-2 text-center">Lorem, ipsum dolor.<br />Lorem, ipsum.</h1>
-            <p className="text-sm text-gray-200 text-center">Lorem ipsum dolor sit amet.</p>
+            <h1 className="text-3xl font-bold leading-tight mb-2 text-center">
+              Lorem, ipsum dolor.
+              <br />
+              Lorem, ipsum.
+            </h1>
+            <p className="text-sm text-gray-200 text-center">
+              Lorem ipsum dolor sit amet.
+            </p>
           </div>
           <div>
-            <p className="text-sm text-center text-black ">Don't have an account?</p>
+            <p className="text-sm text-center text-black">Have an account?</p>
             <button className="mt-2 px-6 py-2 ml-45 bg-white text-black rounded-lg font-semibold text-center hover:bg-gray-200">
-              Register
+              Login
             </button>
           </div>
         </div>
@@ -20,30 +27,32 @@ const SignUp = () => {
         {/* Right Section */}
         <div className="md:w-1/2 w-full p-8 bg-gray-50 flex flex-col justify-center">
           <div className="text-center mb-6">
-            
-            <h2 className="text-2xl font-semibold">Welcome Back!</h2>
+            <h2 className="text-2xl font-semibold">Let Us Know You!</h2>
           </div>
           <form className="flex flex-col gap-4">
+            <input
+              type="text"
+              placeholder="Enter Username"
+              className="p-3 rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-green-400"
+            />
             <input
               type="email"
               placeholder="Enter Your Email"
               className="p-3 rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-green-400"
             />
-            
             <input
               type="password"
               placeholder="Enter Password"
               className="p-3 rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-green-400"
             />
             <button className="bg-green-500 text-white py-3 rounded-lg font-semibold hover:bg-green-600 transition duration-300">
-              Login →
+              Register →
             </button>
           </form>
         </div>
-
       </div>
     </div>
   );
 };
 
-export default SignUp;
+export default SignIn;
