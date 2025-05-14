d4497bca00d235c6add51e70b3387b92c2cc6acc
Shireen Khan <066shiree@gmail.com>
2025-05-14 19:46:36 +0530

    feat: add Navigation component for site navigation

---
+++ src/pages/user/Navigation.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Navigation = () => {
+  return (
+    <div className='w-[30%] bg-amber-200' >
+        <h1>logo</h1>
+        <ul>
+            <li>Home</li>
+            <li>Blogs</li>
+            <li>About Us</li>
+            <li>Contact Us</li>
+        </ul>
+    </div>
+  )
+}
+
+export default Navigation
