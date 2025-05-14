c84929dab14949af5ff4698c20c1d6b709e01f61
Umashnkar Uikey <umashankaruikey325@gmail.com>
2025-05-15 00:17:37 +0530

    feat: enhance Register component with new form layout and styling

--- src/pages/user/Register.jsx
+++ src/pages/user/Register.jsx
@@ -5,6 +5,40 @@ const Register = () => {
     <div className="h-screen flex items-center justify-center bg-gray-900 p-4">
       <div className="min-h-2/3 flex flex-col  md:flex-row w-full max-w-5xl bg-white shadow-xl rounded-2xl overflow-hidden">
         {/* Left Section */}
+          <div className="md:w-1/2 w-full p-8 bg-gray-50 flex flex-col justify-center">
+          <div className="text-center mb-6 ">
+            <h2 className="text-2xl font-semibold font-[SUSE]">
+              Let Us Know You!
+            </h2>
+          </div>
+          <form className="flex flex-col gap-2">
+            <label htmlFor="username">Username:</label>
+            <input
+              type="text"
+              placeholder="Enter Username"
+              className="p-2 rounded-lg border border-gray-300 focus:outline-none focus:ring-1 focus:ring-[#10182874] font-[SUSE]"
+            />
+            <label htmlFor="username">Email:</label>
+            <input
+              type="email"
+              placeholder="Enter Your Email"
+              className="p-2 rounded-lg border border-gray-300 focus:outline-none focus:ring-1 focus:ring-[#10182874] font-[SUSE]"
+            />
+            <label htmlFor="username">Password:</label>
+            <input
+              type="password"
+              placeholder="Enter Password"
+              className="p-2 rounded-lg border border-gray-300 focus:outline-none focus:ring-1 focus:ring-[#10182874] font-[SUSE]"
+            />
+            <button className="bg-[#101828] text-white py-3 rounded-lg font-semibold hover:bg-[#101828a1] transition duration-300 font-[SUSE] cursor-pointer">
+              Register →
+            </button>
+              <p className="text-center font-medium text-lg ">OR</p>
+            <button className="p-1 rounded-lg border border-gray-300 focus:outline-none focus:ring-1 focus:ring-[#10182874] font-[SUSE] font-medium flex justify-center items-center gap-2">
+              <img className="h-12 rounded-full" src="/google_icon.png" alt="" /><p>continue with Google</p>
+            </button>
+          </form>
+        </div>
         <div className="md:w-1/2   text-white flex flex-col p-8 relative justify-around z-10">
           <video
             autoPlay
@@ -40,40 +74,7 @@ const Register = () => {
         </div>
 
         {/* Right Section */}
-        <div className="md:w-1/2 w-full p-8 bg-gray-50 flex flex-col justify-center">
-          <div className="text-center mb-6 ">
-            <h2 className="text-2xl font-semibold font-[SUSE]">
-              Let Us Know You!
-            </h2>
-          </div>
-          <form className="flex flex-col gap-2">
-            <label htmlFor="username">Username:</label>
-            <input
-              type="text"
-              placeholder="Enter Username"
-              className="p-2 rounded-lg border border-gray-300 focus:outline-none focus:ring-1 focus:ring-[#10182874] font-[SUSE]"
-            />
-            <label htmlFor="username">Email:</label>
-            <input
-              type="email"
-              placeholder="Enter Your Email"
-              className="p-2 rounded-lg border border-gray-300 focus:outline-none focus:ring-1 focus:ring-[#10182874] font-[SUSE]"
-            />
-            <label htmlFor="username">Password:</label>
-            <input
-              type="password"
-              placeholder="Enter Password"
-              className="p-2 rounded-lg border border-gray-300 focus:outline-none focus:ring-1 focus:ring-[#10182874] font-[SUSE]"
-            />
-            <button className="bg-[#101828] text-white py-3 rounded-lg font-semibold hover:bg-[#101828a1] transition duration-300 font-[SUSE] cursor-pointer">
-              Register →
-            </button>
-              <p className="text-center font-medium text-lg ">OR</p>
-            <button className="p-1 rounded-lg border border-gray-300 focus:outline-none focus:ring-1 focus:ring-[#10182874] font-[SUSE] font-medium flex justify-center items-center gap-2">
-              <img className="h-12 rounded-full" src="/google_icon.png" alt="" /><p>continue with Google</p>
-            </button>
-          </form>
-        </div>
+      
       </div>
     </div>
   );
