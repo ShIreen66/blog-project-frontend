b380eb1b44a55a55262ded4c3147834004b99b68
Umashnkar Uikey <umashankaruikey325@gmail.com>
2025-05-23 14:32:34 +0530

    feat: enhance AdminNav and AdminHero components with improved layout and functionality; add notification and card elements

--- src/component/AdminHero.jsx
+++ src/component/AdminHero.jsx
@@ -1,9 +1,87 @@
-import React from 'react'
-
+import React from "react";
+import { IoIosNotifications } from "react-icons/io";
+import { RiPagesLine } from "react-icons/ri";
 const AdminHero = () => {
   return (
-    <div className='w-[80%] h-full bg-amber-300'>AdminHero</div>
-  )
-}
+    <div className="w-[80%] h-full bg-amber-300">
+      <div className="top flex justify-between items-center px-10 py-5 ">
+        <input
+          type="text"
+          placeholder="Search"
+          className="border outline-0 px-2 py-1 rounded w-80 text-lg font-medium"
+        />
+        <div className="flex items-center gap-5">
+          <IoIosNotifications className="text-3xl" />
+          <img
+            src="premium_photo.avif"
+            alt=""
+            srcset=""
+            className="h-10 aspect-square object-cover rounded-full"
+          />
+        </div>
+      </div>
+      <div className="cards w-full py-5 px-10 bg-red-300 flex justify-around">
+      
+        <div className="card  h-35 w-60 bg-white rounded-lg shadow-md flex justify-between items-center  overflow-hidden">
+          <div className="left h-full w-[35%] bg-red-400 flex justify-center items-center">
+            <RiPagesLine className="text-4xl" />
+          </div>
+          <div className="right h-full w-[65%] bg-green-400 px-5 py-7  flex flex-col justify-between">
+            <div>
+            
+              <h3 className="text-lg font-medium ">Total Post </h3>
+              <p>120</p>
+            </div>
 
-export default AdminHero
+            <p className="text-sm">View Deatils</p>
+          </div>
+        </div>
+        <div className="card  h-35 w-60 bg-white rounded-lg shadow-md flex justify-between items-center  overflow-hidden">
+          <div className="left h-full w-[35%] bg-red-400 flex justify-center items-center">
+            <RiPagesLine className="text-4xl" />
+          </div>
+          <div className="right h-full w-[65%] bg-green-400 px-5 py-7  flex flex-col justify-between">
+            <div>
+            
+              <h3 className="text-lg font-medium ">Total Post </h3>
+              <p>120</p>
+            </div>
+
+            <p className="text-sm">View Deatils</p>
+          </div>
+        </div>
+        <div className="card  h-35 w-60 bg-white rounded-lg shadow-md flex justify-between items-center  overflow-hidden">
+          <div className="left h-full w-[35%] bg-red-400 flex justify-center items-center">
+            <RiPagesLine className="text-4xl" />
+          </div>
+          <div className="right h-full w-[65%] bg-green-400 px-5 py-7  flex flex-col justify-between">
+            <div>
+            
+              <h3 className="text-lg font-medium ">Total Post </h3>
+              <p>120</p>
+            </div>
+
+            <p className="text-sm">View Deatils</p>
+          </div>
+        </div>
+        <div className="card  h-35 w-60 bg-white rounded-lg shadow-md flex justify-between items-center  overflow-hidden">
+          <div className="left h-full w-[35%] bg-red-400 flex justify-center items-center">
+            <RiPagesLine className="text-4xl" />
+          </div>
+          <div className="right h-full w-[65%] bg-green-400 px-5 py-7  flex flex-col justify-between">
+            <div>
+            
+              <h3 className="text-lg font-medium ">Total Post </h3>
+              <p>120</p>
+            </div>
+
+            <p className="text-sm">View Deatils</p>
+          </div>
+        </div>
+        
+      </div>
+    </div>
+  );
+};
+
+export default AdminHero;
--- src/component/AdminNav.jsx
+++ src/component/AdminNav.jsx
@@ -1,37 +1,56 @@
 import React from "react";
 import { BiCategory, BiComment, BiUser } from "react-icons/bi";
 import { TbBrandGoogleAnalytics } from "react-icons/tb";
+import { CiSettings } from "react-icons/ci";
+import { MdLogout } from "react-icons/md";
 
 const AdminNav = () => {
   return (
-    <div className="w-[20%] h-full bg-green-400">
-      <h2>Logo</h2>
-      <h3>Admin Dashboard</h3>
-      <p className="flex">
-        <i className="ri-add-box-line"></i>
-        <p>Dashboard</p>
-      </p>
-      <p className="flex">
-        <i className="ri-news-line"></i>
-        <p>Posts</p>
-      </p>
-      <p className="flex">
-        <BiCategory />
-        <p>category</p>
-      </p>
-      <p className="flex">
-        <BiComment />
+    <div className="w-[20%] h-full bg-green-400 px-10 py-7 flex flex-col justify-between" >
+      <div >
+        {" "}
+        <h2 className="text-center text-2xl font-medium">Logo</h2>
+        <h3 className="text-center text-2xl font-medium py-3">
+          Admin Dashboard
+        </h3>
+      </div>
 
-        <p>Comments</p>
-      </p>
-      <p className="flex">
-        <BiUser/>
-        <p>Users</p>
-      </p>
-      <p className="flex">
-      <TbBrandGoogleAnalytics />
-        <p>Analytics</p>
-      </p>
+      <div className="flex flex-col gap-2  text-lg  px-5">
+        <p className="flex items-center gap-2">
+          <i className="ri-add-box-line"></i>
+          <p>Dashboard</p>
+        </p>
+        <p className="flex items-center gap-2">
+          <i className="ri-news-line"></i>
+          <p>Posts</p>
+        </p>
+        <p className="flex items-center gap-2">
+          <BiCategory />
+          <p>category</p>
+        </p>
+        <p className="flex items-center gap-2">
+          <BiComment />
+
+          <p>Comments</p>
+        </p>
+        <p className="flex items-center gap-2">
+          <BiUser />
+          <p>Users</p>
+        </p>
+        <p className="flex items-center gap-2">
+          <TbBrandGoogleAnalytics />
+          <p>Analytics</p>
+        </p>
+        <p className="flex items-center gap-2">
+          <CiSettings />
+          <p>Setting</p>
+        </p>
+      </div>
+      <button className="mx-auto flex bg-red-400 items-center gap-2 p-2 rounded">
+        {" "}
+        <MdLogout />
+        <p>Logout</p>
+      </button>
     </div>
   );
 };
